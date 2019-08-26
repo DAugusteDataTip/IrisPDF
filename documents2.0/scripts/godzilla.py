f19cd5b66a624abb16d4a8192b5230bfc0a7a869
T <troberts@paycor.com>
2019-08-26 09:23:12 -0400

    fixed local helpers?

---
+++ documents2.0/scripts/godzilla.py
@@ -0,0 +1,37 @@
+"""
+Here is a docstring about this script.
+
+This is so you know what this script is for or whatever.
+"""
+
+# here are our functions.
+def this_func(num1, num2):
+    """Godzilla has awoken from a long slumber."""
+
+    total = num1 + num2
+    return total
+
+
+def that_func(num1, num2):
+    """Godzilla is here to fight King Ghidorah."""
+
+    result = num1 * num2
+    return result
+
+
+def that_other_func(num1, num2, num3):
+    """Godzilla prepares his atomic breath."""
+
+    result = num1 + num2 + num3
+    return result
+
+def all_hail_the_king(kaiju):
+    """Atomic Godzilla destroys King Ghidorah!"""
+    print("Godzilla defeats " + kaiju)
+    print("Godzilla is the GOAT.")
+
+# if this is main, run these functions
+if __name__ == "__main__":
+    this_func(5, 3)
+    that_func(7, 2)
+    that_other_func(18, 9, 3)
